ee579bff8360f33af8ede2a300028a3797503f57
Tapan08 <tapan.patel0801@gmail.com>
2024-08-03 14:51:31 -0300

    Final-Commit

---
+++ Data-Viewer/StudentsApp/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using StudentsApp.Entities;
+using StudentsApp.Models;
+using StudentsApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace StudentsApp.Controllers
+{
+
+    public class StudentController : Controller
+    {
+        private IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet("/students/{status?}")]
+
+
+        public IActionResult Items(string status)
+        {
+           
+            var students = (status == null || status.ToLower() == "all")
+                           ? _studentService.GetAllStudents()
+                           : _studentService.GetStudentsByStatus(status);
+
+
+            var viewModel = new StudentsViewModel
+            {
+                Students = students,
+                //TabNames = _studentService.GetAllStudents().ToArray(),
+                Active = status ?? "All",
+                New = new Student()
+            };
+
+            return View(viewModel);
+
+        }
+
+        [HttpPost("/students")]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddStudent(string Name, string EnrollmentStatus)
+        {
+            
+            var newS = new Student
+            {
+                Name = Name,
+                EnrollmentStatus = EnrollmentStatus
+            };
+
+            int newSID = _studentService.AddStudent(newS);
+
+            return RedirectToAction("Items", new { status = EnrollmentStatus });
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Data-Viewer/StudentsApp/Models/StudentsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using StudentsApp.Entities;
+
+namespace StudentsApp.Models
+{
+    public class StudentsViewModel
+    {
+        public List<Student> Students = new List<Student>();
+
+        public string[] TabNames = new string[] {"All", "Full-time", "Part-time", "Graduate", "Continuing-Ed" };
+
+        public string[] stat = new string[] { "Full-time", "Part-time", "Graduate", "Continuing-Ed" };
+        public string Active { get; set; }
+        public Student New { get; set; }
+    }
+}
